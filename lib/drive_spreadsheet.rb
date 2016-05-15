3532d34437a4f323f4961d484b561b28a895f667
hsaini734 <hsaini734@gmail.com>
2016-05-15 12:45:42 +0530

    optimising drive_spreadsheet class

--- lib/drive_spreadsheet.rb
+++ lib/drive_spreadsheet.rb
@@ -3,18 +3,17 @@ class DriveSpreadsheet
   attr_reader :worksheets
 
   def initialize(spreadsheet_key)
-    session = google_drive_session
-    @worksheets = get_google_worksheets(session, spreadsheet_key)
+    @worksheets = get_google_worksheets(spreadsheet_key)
   end
 
   private
 
-    def google_drive_session
+    def drive_session
       GoogleDrive.saved_session("config.json")
     end
 
-    def get_google_worksheets(session, spreadsheet_key)
-      session.spreadsheet_by_key(spreadsheet_key).worksheets
+    def get_google_worksheets(spreadsheet_key)
+      drive_session.spreadsheet_by_key(spreadsheet_key).worksheets
     end
 
 end
